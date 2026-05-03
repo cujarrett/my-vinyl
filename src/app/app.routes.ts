2641751e89988124687306fe907aa0f718026552
Matt jarrett <cujarrett@gmail.com>
2026-05-03 02:53:55 -0500

    refactor: remove unnecessary redirect and path for collection route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { Routes } from '@angular/router'
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'collection',
-    pathMatch: 'full',
-  },
-  {
-    path: 'collection',
     loadComponent: () => import('./collection/collection').then((m) => m.Collection),
   },
 ]
